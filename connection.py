e3ad4d1fed324e6ae993009ecf69142afcbf0e37
unknown <deyaniragutierrezarteaga@gmail.com>
2021-04-27 11:12:20 -0600

    Second commit

--- connection.py
+++ connection.py
@@ -1,11 +1,14 @@
 import mysql.connector
-conecction = mysql.connector.connect( host="localhost", user="root", passwd="")
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root", 
+    passwd="")
 
-cursor = conecction.cursor()
+cursor = connection.cursor()
 cursor.execute("show databases")
 
 for base in cursor:
     print(base)
-    
+
 connection.close()  
 
